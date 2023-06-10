25babb7abd7bd636cb0c4f785f52d8c7eb0d6700
elkip <mitchiscool51@gmail.com>
2023-06-10 11:34:32 -0400

    fixed tests

--- src/main/kotlin/com/mitchellhenschel/Application.kt
+++ src/main/kotlin/com/mitchellhenschel/Application.kt
@@ -12,11 +12,8 @@ import io.ktor.server.application.*
 import io.ktor.server.plugins.callloging.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.statuspages.*
-import io.ktor.server.request.*
 import io.ktor.server.response.*
-import io.ktor.util.logging.*
 import mu.KotlinLogging
-import org.slf4j.LoggerFactory
 
 fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain.main(args)
